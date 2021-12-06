39748985174d91d7dfe9c8aee03c88d109d908d1
PALAHANG, Sunshine Richmer D <halimjonik@gmail.compal>
2021-12-06 15:26:57 +0800

    range from 0-9

--- program1.py
+++ program1.py
@@ -1,9 +1,12 @@
 import random
 
 def lottery():
-
+#ask 3 numbers
     userNums = []
     for i in range(3):
-        numbers = int(input("Pick a number from 0 tp 9: "))
-
+        numbers = int(input("Pick a number from 0 to 9: "))
+#no repetition and only from 0 to 9
+        while (numbers in userNums or numbers<0 or numbers>9):
+            print("Invalid. Pick again.")
+            numbers = int(input("Pick a number from 0 to 9: "))
 lottery()
