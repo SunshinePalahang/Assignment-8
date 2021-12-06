9f02c7d990dbb58908dd0fa4897b84555abe2ec0
PALAHANG, Sunshine Richmer D <halimjonik@gmail.compal>
2021-12-06 15:32:17 +0800

    generating 3 random lotto numbers, without repettion

--- program1.py
+++ program1.py
@@ -9,5 +9,12 @@ def lottery():
         while (numbers in userNums or numbers<0 or numbers>9):
             print("Invalid. Pick again.")
             numbers = int(input("Pick a number from 0 to 9: "))
-        userNums.append(numbers)    
+        userNums.append(numbers)  
+#generating 3 random lotto numbers
+    lottoNums = []
+    for j in range (3):
+        numbers = random.randint(0,9)
+#no repition
+    while numbers in lottoNums:
+        numbers = random.randint(0,9)
 lottery()
